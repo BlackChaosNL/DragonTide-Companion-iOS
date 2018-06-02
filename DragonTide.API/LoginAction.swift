ad5edce8b515d81fb8d6d0e87fe44c84c452d4ad
Jeroen Vijgen <jjvijgen@gmail.com>
2018-06-03 00:48:39 +0300

    trying something

--- DragonTide.API/LoginAction.swift
+++ DragonTide.API/LoginAction.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 func loginAction(email: String, password: String, completion: ((_ ok: Bool, _ message: Token?, _ error: Error?) -> Void)?) {
+    setenv("CFNETWORK_DIAGNOSTICS", "3", 1);
     var request = URLRequest(url: URL(string: "https://dragontide.herokuapp.com/auth/login")!);
     request.httpMethod = "POST";
-    request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type");
-    let dataThing = "email=\(email)&password=\(password)".data(using: .utf8);
-    request.httpBody = dataThing;
+    let jsonBody = ["email": email, "password": password];
+    request.httpBody = try? JSONSerialization.data(withJSONObject: jsonBody, options: []);
+    NSLog(String(data: request.httpBody!, encoding: .utf8)!);
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type");
     let session = URLSession(configuration: URLSessionConfiguration.default);
     
     let task = session.dataTask(with: request) { (Data, Response, Error) in
@@ -14,14 +16,6 @@ func loginAction(email: String, password: String, completion: ((_ ok: Bool, _ me
                 completion?(false, nil, error);
             } else if let jsonData = Data {
                 NSLog(String(data: jsonData, encoding: String.Encoding.utf8)!);
-//                let decoder = JSONDecoder();
-//                do {
-//                    //let dec = try decoder.decode([DefaultResponse].type, from: jsonData);
-//                    //completion?(.success(jsonData));
-//                } catch {
-//                    completion?(false, nil, error);
-//                    //completion?(.fail(error));
-//                }
             } else {
                 let err = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data could not be requested"]) as Error;
                 completion?(false, nil, err);
